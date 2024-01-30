dfd2b6deec812df3edf5c60eea44665e0e1fa713
Tom Wu <twu@lcs.on.ca>
2024-01-30 09:59:25 -0500

    Initial Commit

---
+++ Issue314Demo/Issue314DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Issue314DemoApp.swift
+//  Issue314Demo
+//
+//  Created by Tom Wu on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct Issue314DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
